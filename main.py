892d4f0f4efc3fc94c9d2e5c758f98eeb7dda1b8
dadau668 <84313943+dadau668@users.noreply.github.com>
2024-09-26 05:23:08 +0000

    main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def send_email(subject, message):
     msg['From'] = sender_email
     msg['To'] = receiver_email
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP('smtp-mail.outlook.com', 587)
     server.starttls()
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, msg.as_string())
